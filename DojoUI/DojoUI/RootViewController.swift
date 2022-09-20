0ccc41bc83d094c25294ccc84c1739b8e9ce70a6
Alexis Charytonow <alexis@nodeux.com>
2022-09-20 14:09:26 -0500

    Moved to UIView()

--- DojoUI/DojoUI/RootViewController.swift
+++ DojoUI/DojoUI/RootViewController.swift
@@ -7,43 +7,47 @@
 
 import UIKit
 
-class MyUIView: UIViewController {
+class MyUIView: UIView {
     var textField: UITextField!
     var button: UIButton!
     
-    override func viewDidLoad() {
-        let safeArea = view.safeAreaLayoutGuide
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        let safeArea = self.safeAreaLayoutGuide
         
         // UITextField
         textField = UITextField()
-        view.addSubview(textField)
+        self.addSubview(textField)
         textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
-        textField.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
-        textField.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
+        textField.topAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        textField.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 12.0).isActive = true
+        textField.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -12.0).isActive = true
         textField.borderStyle = .line
         
         // UIButton
         button = UIButton()
         button.setTitle("Tap Me!", for: .normal)
         button.setTitleColor(.black, for: .normal)
-        view.addSubview(button)
+        self.addSubview(button)
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.topAnchor.constraint(equalTo: textField.bottomAnchor).isActive = true
         button.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor).isActive = true
-        button.centerYAnchor.constraint(equalTo: safeArea.centerYAnchor).isActive = true
     }
-}
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
+}
 
 class RootViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
-        let myUIView = MyUIView()
-        view.addSubview(myUIView.view)
+        let myUIView = MyUIView(frame: view.frame)
+        view.addSubview(myUIView)
     }
-
 
 }
 
